bec6375fbc02b2c2594121911405a903c61aa252
melekabbassi <abbassimelek@gmail.com>
2022-11-15 10:36:17 +0100

    added new views and controllers

---
+++ StoreWeb/Controllers/CarController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreWeb.Controllers
+{
+    public class CarController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index()
+        {
+            return "Car List";
+        }
+    }
+}
---
+++ StoreWeb/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreWeb.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public String Index()
+        //{
+        //    return "Products List";
+        //}
+
+        //public String NutritionList()
+        //{
+        //    return "Nutrition List";
+        //}
+    }
+}
