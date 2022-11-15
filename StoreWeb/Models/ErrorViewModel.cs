248ab312de1ec1d6cc613fe5f4053488d0d813a8
melekabbassi <abbassimelek@gmail.com>
2022-11-15 10:02:45 +0100

    Add project files.

---
+++ StoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
